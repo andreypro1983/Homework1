c2699b42af228aa0804c8d9ce46450ca9eba793a
Prozhikin_Andrey <a.prozhikin@gmail.com>
2022-11-23 00:15:59 +0200

    feat: Add Task№4

--- Task№2/Program.cs
+++ Task№2/Program.cs
@@ -3,7 +3,7 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-
+Console.Clear();
 Console.Write("Введите первое число: ");
 int NumA=Convert.ToInt32 (Console.ReadLine());
 Console.Write("Введите второе число: ");
